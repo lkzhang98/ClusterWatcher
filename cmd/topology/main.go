f35b38750bae02c0208d2f4578fe5b83b8491723
lkzhang17 <lkzhang98@163.com>
2023-08-21 00:18:47 +0800

    basic code commit

--- cmd/topology/main.go
+++ cmd/topology/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"ClusterWatcher/internal/topology"
+	"os"
+)
+
+func main() {
+	command := topology.NewTopologyCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
